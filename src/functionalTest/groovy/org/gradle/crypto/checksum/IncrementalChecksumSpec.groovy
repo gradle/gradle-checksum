f5a145700a039dd653d50381b4f65816f8f9de44
Jerome Prinet <jprinet@gradle.com>
2022-02-09 12:06:35 +0100

    Add caching tests
    Signed-off-by: Jerome Prinet <jprinet@gradle.com>

--- src/functionalTest/groovy/org/gradle/crypto/checksum/IncrementalChecksumSpec.groovy
+++ src/functionalTest/groovy/org/gradle/crypto/checksum/IncrementalChecksumSpec.groovy
@@ -16,7 +16,7 @@ class IncrementalChecksumSpec extends Specification {
         newFile('settings.gradle')
     }
 
-    def 'can handle changing file trees on #gradleVersion with configuration cache = #isConfigurationCacheEnabled'() {
+    def 'can handle changing file trees'() {
         given:
         newFile('build.gradle') << """
             plugins {
@@ -37,7 +37,7 @@ class IncrementalChecksumSpec extends Specification {
         newFile('input/bar.txt') << 'bar'
 
         when:
-        def firstResult = build(gradleVersion, isConfigurationCacheEnabled)
+        def firstResult = build('checksum')
 
         then:
         firstResult.task(':checksums').getOutcome() == TaskOutcome.SUCCESS
@@ -46,38 +46,18 @@ class IncrementalChecksumSpec extends Specification {
         newFolder('input', 'subdir')
         newFile('input/subdir/sub-foo.txt') << 'sub-foo'
         newFile('input/baz.txt') << 'baz'
-        def secondResult = build(gradleVersion, isConfigurationCacheEnabled)
+        def secondResult = build('checksum')
 
         then:
         secondResult.task(':checksums').getOutcome() == TaskOutcome.SUCCESS
         file('checksums/sub-foo.txt.sha256').exists()
         file('checksums/baz.txt.sha256').exists()
-        if(isConfigurationCacheEnabled) {
-            secondResult.output.contains("Reusing configuration cache.")
-        }
-
-        where:
-        gradleVersion | isConfigurationCacheEnabled
-        '4.0'         | false
-        '5.0'         | false
-        '6.6'         | false
-        '6.6'         | true
-        '7.4'         | false
-        '7.4'         | true
     }
 
-    private BuildResult build(String gradleVersion, boolean isConfigurationCacheEnabled) {
-        List<String> args = new ArrayList<>()
-        if(isConfigurationCacheEnabled) {
-            args.add('--configuration-cache')
-        }
-        args.add('--stacktrace')
-        args.add('checksum')
-
+    private BuildResult build(String args) {
         GradleRunner.create()
-                .withGradleVersion(gradleVersion)
                 .withProjectDir(projectDir.root)
-                .withArguments(args)
+                .withArguments('--stacktrace', args)
                 .withPluginClasspath()
                 .build()
     }
